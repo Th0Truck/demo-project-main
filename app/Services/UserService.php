adaf3efd2936cae0836a5c94e8b522429a48ee71
Th0Truck <thotruck@gmail.com>
2025-08-07 10:27:34 +0200

    UserService

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -14,35 +14,80 @@ readonly class UserService
     ) {}
 
     /**
+     * Adds a new user with the provided details and creates an account for them.
+     * @param string $fullName
+     * @param string $email
+     * @param string $merchantId
+     * @return UserDto
+     * 
      * @throws Exception
      */
     public function addUser(string $fullName, string $email, string $merchantId): UserDto {
+        $user = $this->createUser($fullName, $email, $merchantId);
+        $this->accountService->addAccountForUser($user->userId);
+
+        return $this->mapToDto($user);
+    }
+
+    /**
+     * Creates a new user with the given details.
+     * 
+     * @return User
+     * @throws Exception
+     */
+    private function createUser(string $fullName, string $email, string $merchantId): User
+    {
         $user = new User([
             'userId' => Uuid::uuid4(),
             'fullName' => $fullName,
             'email' => $email,
-            'merchantId' => $merchantId
+            'merchantId' => $merchantId,
         ]);
 
         $user->save();
-        $this->accountService->addAccountForUser($user->userId);
-        return $this->userToUserDto($user);
+
+        return $user;
     }
 
     /**
+     * Retrieves a user by their ID and maps it to a UserDto.
+     * 
      * @throws Exception
      */
-    public function getUser(string $userId): UserDto {
+    public function getUser(string $userId): UserDto
+    {
+        $user = $this->findUserById($userId);
+
+        return $this->mapToDto($user);
+    }
+
+    /**
+     * Find the user by their ID.
+     * 
+     * @return User
+     * @throws Exception
+     */
+    public function findUserById(string $userId): User {
         $user = User::query()->where('userId', $userId)->first();
 
         if (is_null($user)) {
             throw new Exception("User with id " . $userId . " not found");
         }
 
-        return $this->userToUserDto($user);
+        return $user;
     }
 
-    private function userToUserDto(User $user): UserDto {
-        return new UserDto($user->userId, $user->fullName, $user->email);
+    /**
+     * Maps a User model to a UserDto.
+     * 
+     * @return UserDto
+     */
+    private function mapToDto(User $user): UserDto
+    {
+        return new UserDto(
+            $user->userId,
+            $user->fullName,
+            $user->email
+        );
     }
 }
