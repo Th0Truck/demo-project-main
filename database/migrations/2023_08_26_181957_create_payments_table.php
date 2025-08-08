cd791ca01b6a498cf99e14b5d9a7643be5e0564c
Th0Truck <thotruck@gmail.com>
2025-08-08 11:07:02 +0200

    Unique contraints in the way

--- database/migrations/2023_08_26_181957_create_payments_table.php
+++ database/migrations/2023_08_26_181957_create_payments_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->string('paymentId')->unique();
-            $table->string('userId')->unique();
-            $table->string('merchantId')->unique();
+            $table->string('userId');
+            $table->string('merchantId');
             $table->float('amount');
         });
     }
